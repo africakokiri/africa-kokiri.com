6272f17e44844c8436a68ec8102f26b5ecad0431
Choi Chan Hwan <africakokiri@gmail.com>
2025-02-21 13:05:53 +0900

    Init

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import NextThemesProvider from "@/libs/next-themes/next-themes-provider";
+import "@/styles/globals.css";
+
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Africa Kokiri",
+  icons: {
+    icon: "/icons/kokiri.svg"
+  }
+};
+
+export default function RootLayout({
+  children
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="ko">
+      <body className="h-screen w-screen antialiased">
+        <NextThemesProvider>{children}</NextThemesProvider>
+      </body>
+    </html>
+  );
+}
